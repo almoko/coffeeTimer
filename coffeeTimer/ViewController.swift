e1a6e70b6b46090214cdf0b0879ee54e52934a75
Alex Motrenko <almoko@gmail.com>
2015-07-31 23:05:08 +0300

    Initial Commit

---
+++ coffeeTimer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coffeeTimer
+//
+//  Created by Alex on 7/31/15.
+//  Copyright © 2015 Alex Motrenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
